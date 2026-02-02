ecd07ba9af18802b1053171a6a91918d4c3a61e2
Ventus <aosica789@gmail.com>
2026-02-02 12:16:32 +0100

    Add MonstersController with GET endpoint

---
+++ GameEncyclopedia/Controllers/MonstersController.cs
@@ -0,0 +1,30 @@
+﻿using GameEncyclopedia.Data;
+using GameEncyclopedia.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameEncyclopedia.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MonstersController : ControllerBase {
+        private readonly GameEncyclopediaDbContext _db;
+
+        public MonstersController(GameEncyclopediaDbContext db) {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<MonsterResponse>>> GetAllMonsters(CancellationToken ct) {
+            var monsters = await _db.Monsters
+                .AsNoTracking()
+                .OrderBy(m => m.Id)
+                .Select(m => new MonsterResponse {
+                    Id = m.Id,
+                    Name = m.Name,
+                })
+                .ToListAsync(ct);
+
+            return Ok(monsters);
+        }
+    }
+}
