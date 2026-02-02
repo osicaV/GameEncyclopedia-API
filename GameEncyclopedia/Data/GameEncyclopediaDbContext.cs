2fc64abbd904e6986399a51f81e31920b2812bd0
Ventus <aosica789@gmail.com>
2026-02-02 11:40:29 +0100

    Add Monster entity and DbContext

---
+++ GameEncyclopedia/Data/GameEncyclopediaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using GameEncyclopedia.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameEncyclopedia.Data {
+    public class GameEncyclopediaDbContext : DbContext {
+        public GameEncyclopediaDbContext(DbContextOptions<GameEncyclopediaDbContext> options) : base(options) {
+            
+        }
+
+        public DbSet<Monster> Monsters => Set<Monster>();
+    }
+}
---
+++ GameEncyclopedia/Entities/Monster.cs
@@ -0,0 +1,6 @@
+﻿namespace GameEncyclopedia.Entities {
+    public class Monster {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+}
