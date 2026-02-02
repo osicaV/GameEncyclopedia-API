254961f60c53e033cf44dd6d6a16e18c1e358b1d
Ventus <aosica789@gmail.com>
2026-02-02 11:53:01 +0100

    Add initial EF Core migration

--- GameEncyclopedia/Program.cs
+++ GameEncyclopedia/Program.cs
@@ -1,10 +1,13 @@
+using GameEncyclopedia.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<GameEncyclopediaDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GameEncyclopedia")));
 
 var app = builder.Build();
 
