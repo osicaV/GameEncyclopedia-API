5cc04b5511e26304706e6a8284e7902bcc9d00b4
Ventus <aosica789@gmail.com>
2026-02-03 14:14:17 +0100

    Add POST /api/monsters

--- GameEncyclopedia/Controllers/MonstersController.cs
+++ GameEncyclopedia/Controllers/MonstersController.cs
@@ -1,5 +1,6 @@
 ﻿using GameEncyclopedia.Data;
 using GameEncyclopedia.Dtos;
+using GameEncyclopedia.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -28,7 +29,7 @@ namespace GameEncyclopedia.Controllers {
         }
 
         [HttpGet("id:int")]
-        public async Task<ActionResult<List<MonsterResponse>>> GetAllMonsters(int id, CancellationToken ct) {
+        public async Task<ActionResult<List<MonsterResponse>>> GetMonsterById(int id, CancellationToken ct) {
             var monster = await _db.Monsters
                 .AsNoTracking()
                 .Where(m => m.Id == id)
@@ -41,6 +42,23 @@ namespace GameEncyclopedia.Controllers {
             if (monster is null) return NotFound();
 
             return Ok(monster);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MonsterResponse>> Create(CreateMonsterRequest request) {
+            var monster = new Monster {
+                Name = request.Name.Trim()
+            };
+
+            _db.Monsters.Add(monster);
+            await _db.SaveChangesAsync();
+
+            var response = new MonsterResponse {
+                Id = monster.Id,
+                Name = monster.Name
+            };
+
+            return CreatedAtAction(nameof(GetMonsterById), new { id = monster.Id }, response);
         }
     }
 }
---
+++ GameEncyclopedia/Dtos/CreateMonsterRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameEncyclopedia.Dtos {
+    public class CreateMonsterRequest {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string Name { get; set; } = default!;
+    }
+}
