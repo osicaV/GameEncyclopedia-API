c0271e9a9ffe0a4b9d06491df82f28eacf37f7b7
Ventus <aosica789@gmail.com>
2026-02-05 23:28:28 +0100

    Assert JSON response for GET /api/monsters

--- GameEncyclopedia.IntegrationTests/MonstersTest.cs
+++ GameEncyclopedia.IntegrationTests/MonstersTest.cs
@@ -1,16 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using GameEncyclopedia.Dtos;
+using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
+using System.Net.Http.Json;
 
 namespace GameEncyclopedia.IntegrationTests {
     public class MonstersTest {
         [Test]
-        public async Task Get_Monsters_ReturnsOk() {
+        public async Task Get_Monsters_ReturnsOk_AndJsonArray() {
             await using var factory = new WebApplicationFactory<Program>();
             using var client = factory.CreateClient();
 
             var response = await client.GetAsync("/api/Monsters");
 
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var monsters = await response.Content.ReadFromJsonAsync<List<MonsterResponse>>();
+            Assert.That(monsters, Is.Not.Null);
         }
     }
 }
